175e8e6e9db931ad13fb558e1181aa53d7725a11
Ahmet Ceylan <dev.ahmetceylan@gmail.com>
2023-05-31 06:32:46 +0300

    final edit

--- lib/app/modules/detail/widgets/done_list.dart
+++ lib/app/modules/detail/widgets/done_list.dart
@@ -1,10 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:todo_app_getx/app/core/utils/extensions.dart';
 import 'package:todo_app_getx/app/core/values/colors.dart';
 import 'package:todo_app_getx/app/modules/home/controller.dart';
